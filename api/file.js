4719f470d072ab03e5bb65bcf1c90bd59227e6f0
Leandro Duarte Silva <82515067+LeandroDuarte28@users.noreply.github.com>
2026-05-01 11:44:26 -0300

    Create file.js

---
+++ api/file.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const fileUrl = "https://raw.githubusercontent.com/LeandroDuarte28/DocsGraz/main/DECLARAÇÃO%20DE%20ENDEREÇO-2.pdf";
+
+  const response = await fetch(fileUrl);
+
+  if (!response.ok) {
+    return res.status(500).send("Erro ao baixar arquivo");
+  }
+
+  const buffer = await response.arrayBuffer();
+
+  res.setHeader("Content-Type", "application/octet-stream");
+  res.setHeader("Content-Disposition", "attachment; filename=declaracao.pdf");
+  res.setHeader("Cache-Control", "no-store");
+
+  res.send(Buffer.from(buffer));
+}
